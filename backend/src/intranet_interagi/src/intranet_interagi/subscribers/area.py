dc4c941b2157c867f7327840d40ccb1b09c9014d
Érico Andrei <ericof@gmail.com>
2023-05-13 14:48:30 -0300

    Corrige nome do campo exclude_from_nav

--- backend/src/intranet_interagi/src/intranet_interagi/subscribers/area.py
+++ backend/src/intranet_interagi/src/intranet_interagi/subscribers/area.py
@@ -8,8 +8,8 @@ from zope.lifecycleevent import ObjectModifiedEvent
 def _update_excluded_from_nav(obj: Area):
     """Update excluded_from_nav in the Area object."""
     predio = obj.predio
-    obj.excluded_from_nav = False if predio else True
-    logger.info(f"Atualizado o campo excluded_from_nav para {obj.title}")
+    obj.exclude_from_nav = False if predio else True
+    logger.info(f"Atualizado o campo exclude_from_nav para {obj.title}")
 
 
 def _create_user_groups(obj: Area):
