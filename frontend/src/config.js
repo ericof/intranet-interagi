dfac108cdb3741d5f2270fd086ee6b690eab1aa7
Érico Andrei <ericof@gmail.com>
2023-05-20 21:24:16 +0200

    Exercício 47

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -12,6 +12,7 @@
  *    }
  * }
  */
+import AreaView from './components/View/AreaView';
 // All your imports required for the config here BEFORE this line
 import '@plone/volto/config';
 
@@ -23,5 +24,9 @@ export default function applyConfig(config) {
     defaultLanguage: 'pt-br',
   };
   config.blocks.blocksConfig.codeBlock.defaultStyle = 'light';
+  config.views.contentTypesViews = {
+    ...config.views.contentTypesViews,
+    Area: AreaView,
+  };
   return config;
 }
