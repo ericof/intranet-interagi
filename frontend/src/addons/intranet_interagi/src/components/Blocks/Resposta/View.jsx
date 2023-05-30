cad96a6143f09e690bc5117fe5b569d26f79fd63
Érico Andrei <ericof@gmail.com>
2023-05-30 19:31:27 -0300

    Exercício 73

--- frontend/src/addons/intranet_interagi/src/components/Blocks/Resposta/View.jsx
+++ frontend/src/addons/intranet_interagi/src/components/Blocks/Resposta/View.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const RespostaView = (props) => {
   return (
-    <div>
-      <h3>42</h3>
+    <div className={'block blockResposta'}>
+      <div className={'container'}>
+        <h3 className={'value'}>42</h3>
+      </div>
     </div>
   );
 };
